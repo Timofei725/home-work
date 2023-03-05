0ec9afebd651658a705a83d191a60d1a59047b77
Timofei725 <timofei725@yandex.ru>
2023-03-05 15:11:15 +0300

    HW1

---
+++ Stars.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Stars {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            String template = scanner.next();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(template);
+                    if (j != m - 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
